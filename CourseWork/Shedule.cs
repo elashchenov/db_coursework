30496c3ea0ae34438bb6542090a8b42a57ff5bfa
Evgeny Lashchenov <latshenov.ei@edu.spbstu.ru>
2019-11-13 21:18:22 +0300

    Add load from database for shedules

--- CourseWork/Shedule.cs
+++ CourseWork/Shedule.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace CourseWork
 {
@@ -19,29 +20,74 @@ namespace CourseWork
             //shedule_table.GetControlFromPosition(1, 1);
             //shedule_table.Controls.Add();
             //List<ReadOnlyTextBox> sheduleArray = new List<ReadOnlyTextBox>();
-            
+            OleDbConnection connection = new OleDbConnection("Provider=SQLNCLI11; Data Source=localhost;Persist Security Info=True;User ID=sa;Password=1234;Initial Catalog=school");
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter();
+
+            try
+            {
+                connection.Open();
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Coonected failed!");
+            }
+
             for (int dayIdx = 1; dayIdx <= 6; dayIdx++)
             {
                 for (int lessIdx = 1; lessIdx <= 7; lessIdx++)
                 {
                     ReadOnlyTextBox newTextBox = new ReadOnlyTextBox();
-                    //newTextBox.Margin = new Padding(1, 1, 0, 0);
                     newTextBox.Font = new Font("Comic Sans MS", 10);
                     newTextBox.Dock = DockStyle.Fill;
                     newTextBox.Multiline = true;
                     newTextBox.BorderStyle = BorderStyle.None;
                     newTextBox.WordWrap = false;
                     List<String> lines = new List<String>();
-                    lines.Add("Предмет");
-                    lines.Add("Преподаватель");
-                    lines.Add("Кабинет");
-                    newTextBox.Lines = lines.ToArray();
+                    DataTable table = new DataTable();
+                    String day = "";
+
+                    switch (dayIdx)
+                    {
+                        case 1:
+                            day = "mon";
+                            break;
+                        case 2:
+                            day = "tue";
+                            break;
+                        case 3:
+                            day = "wen";
+                            break;
+                        case 4:
+                            day = "thur";
+                            break;
+                        case 5:
+                            day = "fri";
+                            break;
+                        case 6:
+                            day = "sat";
+                            break;
+                    }
+
+                    OleDbCommand command = new OleDbCommand("select subjects.name from puples, shedules, subjects where puples.classId = shedules.classId and shedules.subjectId = subjects.id and shedules.day = '" + day + "' and puples.id = 5 and shedules.subjectNumber = '" + lessIdx + "'", connection);
+                    adapter.SelectCommand = command;
+                    adapter.Fill(table);
+
+                    foreach (DataRow row in table.Rows)
+                    {
+                        string name = row["name"].ToString();
+                        newTextBox.Text = name;
+
+                    }
+
                     Control controlForDelete = shedule_table.GetControlFromPosition(lessIdx, dayIdx);
                     shedule_table.Controls.Remove(controlForDelete);
                     shedule_table.Controls.Add(newTextBox, lessIdx, dayIdx);
-                    //sheduleArray.Add(newTextBox, 1, 2);
+
                 }
             }
+            connection.Close();
 
         }
 
